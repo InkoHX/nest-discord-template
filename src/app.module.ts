f4fe07c4c65ad461a35e16c234e397de5fca0c33
InkoHX <me@inkohx.dev>
2021-01-04 18:36:48 +0900

    add DiscordCommandModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { DiscordCommandModule } from './discord-command/discord-command.module'
 import { DiscordModule } from './discord/discord.module'
 
 @Module({
-  imports: [DiscordModule],
+  imports: [DiscordModule, DiscordCommandModule],
   controllers: [AppController],
   providers: [AppService],
 })
