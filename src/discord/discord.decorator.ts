becaf1b75b5e70442f969159ccfd814d23aceae2
InkoHX <me@inkohx.dev>
2021-01-04 22:36:15 +0900

    add DiscordCommand decorators

--- src/discord/discord.decorator.ts
+++ src/discord/discord.decorator.ts
@@ -1,9 +1,75 @@
 import { SetMetadata } from '@nestjs/common'
 import { ClientEvents } from 'discord.js'
 
+import {
+  DiscordCommandArgumentMetadata,
+  DiscordCommandMessageMetadata,
+  DiscordCommandParamMetadata,
+  DiscordCommandParamType,
+} from './discord.interface'
+
 export const DISCORD_EVENT = Symbol('DISCORD_EVENT')
+export const DISCORD_COMMAND = Symbol('DISCORD_COMMAND')
+export const DISCORD_COMMAND_SIMPLE = Symbol('DISCORD_COMMAND_SIMPLE')
+export const DISCORD_COMMAND_ARGUMENT = Symbol('DISCORD_COMMAND_ARGUMENT')
+export const DISCORD_COMMAND_PARAM = Symbol('DISCORD_COMMAND_PARAM')
 
 export const DiscordEvent = (
   eventName: keyof ClientEvents,
   once = false
 ): MethodDecorator => SetMetadata(DISCORD_EVENT, { eventName, once })
+
+const createCommandParamDecorator = (paramType: DiscordCommandParamType) => (
+  argumentName?: string
+): ParameterDecorator => (target, propertyKey, parameterIndex) => {
+  const metadata: DiscordCommandParamMetadata[] =
+    Reflect.getMetadata(
+      DISCORD_COMMAND_PARAM,
+      target.constructor,
+      propertyKey
+    ) ?? []
+
+  if (paramType === 'MESSAGE') {
+    Reflect.defineMetadata(
+      DISCORD_COMMAND_PARAM,
+      [
+        ...metadata,
+        {
+          paramType,
+          parameterIndex,
+        } as DiscordCommandMessageMetadata,
+      ],
+      target.constructor,
+      propertyKey
+    )
+  } else {
+    Reflect.defineMetadata(
+      DISCORD_COMMAND_PARAM,
+      [
+        ...metadata,
+        {
+          paramType,
+          argumentName,
+          parameterIndex,
+        } as DiscordCommandArgumentMetadata,
+      ],
+      target.constructor,
+      propertyKey
+    )
+  }
+}
+
+export const Command = (commandName: string): ClassDecorator =>
+  SetMetadata(DISCORD_COMMAND, commandName)
+
+export const SingleCommand = (
+  commandName: string,
+  commandArgs: string
+): MethodDecorator =>
+  SetMetadata(DISCORD_COMMAND_SIMPLE, { commandName, commandArgs })
+
+export const CommandArgument = (commandArgs: string): MethodDecorator =>
+  SetMetadata(DISCORD_COMMAND_ARGUMENT, commandArgs)
+
+export const CommandParam = createCommandParamDecorator('ARGUMENT')
+export const CommandMessage = createCommandParamDecorator('MESSAGE')()
--- src/discord/discord.interface.ts
+++ src/discord/discord.interface.ts
@@ -6,3 +6,19 @@ export interface DiscordEventMetadata {
 export interface DiscordEvent extends DiscordEventMetadata {
   callback: (...args: any[]) => void
 }
+
+export type DiscordCommandParamType = 'ARGUMENT' | 'MESSAGE'
+
+export type DiscordCommandMessageMetadata = {
+  paramType: 'MESSAGE'
+  parameterIndex: number
+}
+
+export type DiscordCommandArgumentMetadata = {
+  paramType: 'ARGUMENT'
+  parameterIndex: number
+  argumentName: string
+}
+
+export type DiscordCommandParamMetadata = DiscordCommandMessageMetadata &
+  DiscordCommandArgumentMetadata
