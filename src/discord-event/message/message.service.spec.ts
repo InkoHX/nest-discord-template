789f2f9e0732888aeaf8f446f2c52046f0d8a06c
InkoHX <me@inkohx.dev>
2021-01-07 10:50:54 +0900

    test: message.service

--- src/discord-event/message/message.service.spec.ts
+++ src/discord-event/message/message.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing'
 
+import { DiscordModule } from '../../discord/discord.module'
 import { MessageService } from './message.service'
 
 describe('MessageService', () => {
@@ -8,6 +9,7 @@ describe('MessageService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [MessageService],
+      imports: [DiscordModule],
     }).compile()
 
     service = module.get<MessageService>(MessageService)
