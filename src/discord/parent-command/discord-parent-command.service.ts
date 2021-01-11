0098346968557e41328421b2669d84555e01dbe9
InkoHX <me@inkohx.dev>
2021-01-12 01:36:27 +0900

    refactor: scan method

--- src/discord/parent-command/discord-parent-command.service.ts
+++ src/discord/parent-command/discord-parent-command.service.ts
@@ -1,7 +1,11 @@
 import { Injectable, Type } from '@nestjs/common'
 import { DiscoveryService, MetadataScanner } from '@nestjs/core'
 
-import { DiscordChildCommand, DiscordParentCommand } from '../discord.interface'
+import {
+  DiscordChildCommand,
+  DiscordChildCommandMetadata,
+  DiscordParentCommand,
+} from '../discord.interface'
 import { DiscordReflectorService } from '../reflector/discord-reflector.service'
 
 @Injectable()
@@ -27,29 +31,33 @@ export class DiscordParentCommandService extends Set<DiscordParentCommand> {
     metaType: Function | Type<any>
   ): DiscordParentCommand | null {
     const commandName = this.reflector.getParentCommandMetadata(metaType)
-    const children: DiscordChildCommand[] = []
 
     if (!commandName) return null
 
-    this.metadataScanner.scanFromPrototype(
+    const methods = this.metadataScanner.scanFromPrototype(
       instance,
       metaType.prototype,
-      name => {
-        const childMetadata = this.reflector.getChildCommandMetadata(
-          instance[name]
-        )
-        const params = this.reflector.getCommandParamMetadata(metaType, name)
-
-        if (!childMetadata) return
-
-        children.push({
-          callback: (instance[name] as Type<any>).bind(instance),
-          commandArgs: childMetadata.commandArgs,
-          commandName: childMetadata.commandName,
-          params,
-        })
-      }
+      name => name
     )
+    const children = methods
+      .map(methodName => ({
+        methodName,
+        childMetadata: this.reflector.getChildCommandMetadata(
+          instance[methodName]
+        ),
+      }))
+      .filter(metadata => metadata.childMetadata)
+      .map(({ methodName, childMetadata }) => ({
+        methodName,
+        childMetadata: childMetadata as DiscordChildCommandMetadata,
+        params: this.reflector.getCommandParamMetadata(metaType, methodName),
+      }))
+      .map<DiscordChildCommand>(({ childMetadata, params, methodName }) => ({
+        callback: (instance[methodName] as Type<any>).bind(instance),
+        commandName: childMetadata.commandName,
+        commandArgs: childMetadata.commandArgs,
+        params,
+      }))
 
     return {
       commandName,
